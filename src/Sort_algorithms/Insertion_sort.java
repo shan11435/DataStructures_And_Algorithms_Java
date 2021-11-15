45b58a0b9d569abe26b29cf9cf8fc29a42ff4d9f
Tahsin Ahmed <shanahmed1997@gmail.com>
2021-11-15 17:26:04 -0500

    added comments

--- src/Sort_algorithms/Insertion_sort.java
+++ src/Sort_algorithms/Insertion_sort.java
@@ -6,16 +6,21 @@ public class Insertion_sort {
         //these are the elements for the intArray array
         int[] intArray = {20, 35, -15, 7, 55, 1, -22};
 
+        //this will loop through the first element that's not sorted, when loop is first started length = 1
         for(int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++)
         {
+            //new element is assigned to 35
             int newElement = intArray[firstUnsortedIndex];
             int i;
 
+            //based on the loop, if the index meets all these conditions then execute the inside of the loop
             for(i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--)
             {
+                //the index will be swapped with the previous index
                 intArray[i] = intArray[i - 1];
             }
 
+            //the index will be assigned the index in newElement variable
             intArray[i] = newElement;
         }
 
